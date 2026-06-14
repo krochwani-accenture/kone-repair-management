0ac29bb746174f5aa3c5ffd37c650a26e7166eb4
krochwani-accenture <kratika.rochwani@accenture.com>
2026-06-14 17:42:19 +0530

    ecosystem config file

--- .aws-sam/backend-ecosystem-config.js
+++ .aws-sam/backend-ecosystem-config.js
@@ -3,13 +3,13 @@ module.exports = {
     {
       name: "backend",
       script: "./backend/server.js",
-      cwd: "/home/ec2-user/kone-repair-management/backend",
+      cwd: "/home/ec2-user/kone-repair-management",
       instances: 1,
       exec_mode: "fork",
       autorestart: true,
       watch: false,
       env: {
-        NODE_ENV: "production",
+        NODE_ENV: "product  ion",
         PORT: 5000
       }
     }
